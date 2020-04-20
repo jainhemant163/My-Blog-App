cf554d3a22b11849124ae8e8c9c77fa6e2f400c8
Hemant Jain <jainhemant163@gmail.com>
2020-04-20 12:47:39 -0400

    First commit

---
+++ src/build/precache-manifest.adafacf082deb7d6bfb8af37cb0a9929.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a4cb749d5a4179673e3",
+    "url": "/static/css/main.a57042c6.chunk.css"
+  },
+  {
+    "revision": "5a4cb749d5a4179673e3",
+    "url": "/static/js/main.75bfb979.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e3411b17d3915443b45c",
+    "url": "/static/js/2.4a7f71bf.chunk.js"
+  },
+  {
+    "revision": "2a25db7b8565d389080e27883685507c",
+    "url": "/index.html"
+  }
+];
